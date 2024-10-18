a43ae7467c1f6239639973fc98ddd51a13fe1542
Ioan <ioan.panaite77@outlook.com>
2024-10-18 01:10:05 -0400

    react redux project

---
+++ src/features/product/productService.ts
@@ -0,0 +1,41 @@
+import axiosConfig from "../../app/axiosConfig";
+import { Product } from "../../types/product";
+
+const getProducts = async () => {
+  const response = await axiosConfig.get("printful/store/products");
+
+  return response.data.result;
+};
+const getSingleProduct = async (id: number) => {
+  const response = await axiosConfig.get(`printful/store/products/${id}`);
+  console.log("This is single product response", response.data.result.sync_product.thumbnail_url)
+  return response.data.result;
+
+ 
+};
+
+const getCategory = async (category: any) => {
+  const products = await getProducts();
+  const filteredProducts = products.filter((product: Product) =>
+    product.name.toLowerCase().includes(category.toLowerCase())
+  );
+  return filteredProducts;
+};
+
+const searchProducts = async (query: any) => {
+  const products = await getProducts();
+  const filteredProducts = products.filter((product: Product) =>
+    product.name.toLowerCase().includes(query.toLowerCase())
+  );
+  console.log("Hello Guys", filteredProducts)
+  return filteredProducts;
+};
+
+const productService = {
+  getProducts,
+  getSingleProduct,
+  getCategory,
+  searchProducts,
+};
+
+export default productService;
